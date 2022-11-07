5f1cf7d03c0999ea377b09c8388a635efee2551b
mateussdev <mateusbs427@gmail.com>
2022-11-07 20:33:13 -0300

    add banner

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ const StyledHeader = styled.div`
 function Header() {
   return (
     <StyledHeader>
-      <img className="banner" src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+      <img className="banner" src={config.banner} />
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
